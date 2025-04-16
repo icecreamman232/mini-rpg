c7714895b321b393f354617a0163d360ea4f08bd
Stephen Ho <stephen.ho.232@gmail.com>
2025-04-17 01:43:34 +0700

    Update sprite and control

--- mini-rpg-project/Script/Player/player_movement.gd
+++ mini-rpg-project/Script/Player/player_movement.gd
@@ -1,49 +1,41 @@
 class_name PlayerMovement extends CharacterBody2D
 
-enum MOVING_STATE {IDLE, MOVING}
-
 @export var move_direction:Vector2
 @export var move_speed:float
-@export var moving_state:MOVING_STATE
+@export var anim:AnimatedSprite2D
+@export var is_face_left:bool
 
-const cell_size:float = 16
-var next_pos:Vector2
-var prev_pos:Vector2
-var lerpValue:float
+const idle_anim = "idle"
+const run_anim = "run"
 
 func _update_input():
-
-    if moving_state == MOVING_STATE.MOVING: return
-
     move_direction = Vector2i.ZERO
 
     if Input.is_action_pressed("move_down"):
         move_direction.y = 1
-    elif Input.is_action_pressed("move_up"):
+    if Input.is_action_pressed("move_up"):
         move_direction.y = -1
-    elif Input.is_action_pressed("move_left"):
+    if Input.is_action_pressed("move_left"):
         move_direction.x = -1
-    elif Input.is_action_pressed("move_right"):
+    if Input.is_action_pressed("move_right"):
         move_direction.x = 1
 
+
+func _update_movement(delta:float):
+    position += move_direction * delta * move_speed
+
+func _update_anim():
     if move_direction.length() > 0:
-        moving_state = MOVING_STATE.MOVING
-        next_pos = position + move_direction * cell_size
-        prev_pos = position
-
-func _update_movment(delta:float):
-
-    if moving_state == MOVING_STATE.IDLE: return
-
-    if lerpValue <1:
-        position = lerp(prev_pos,next_pos,lerpValue)
-        lerpValue += move_speed * delta
+        anim.play(run_anim)
     else:
-        move_direction = Vector2.ZERO
-        moving_state = MOVING_STATE.IDLE
-        lerpValue = 0
+        anim.play(idle_anim)
+    
+    if move_direction.length() > 0:
+        anim.flip_h = move_direction.x > 0
+        is_face_left = move_direction.x <= 0
 
 func _process(delta: float) -> void:
     _update_input()
-    _update_movment(delta)
+    _update_movement(delta)
+    _update_anim()
     
